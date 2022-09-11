83d1e52d10f04cc8561a57aefd71574b7c7e7f60
Daniel Gómez <88523667+Danigom11@users.noreply.github.com>
2022-09-11 19:33:34 +0200

    Ejercicios 1 y 2 de Open Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Ejercicio 1
+        int resultadoSuma;
+        resultadoSuma = sumaTresNumeros(10, 20, 30);
+        System.out.println("Suma de los tres numeros: " + resultadoSuma);
+
+        // Ejercicio 2
+        Coche miCoche = new Coche();
+        miCoche.AnadirPuerta();
+        System.out.println("Número de puertas de mi coche: " + miCoche.puertas);
+    }
+
+    // Ejercicio 1. Función con tres parámetros.
+    public static int sumaTresNumeros(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+// Ejercicio 2. Crear clase coche
+class Coche {
+    public int puertas = 4;
+
+    public void AnadirPuerta() {
+        this.puertas++;
     }
 }
